839acd31dad7f866126e118fc3ef16eca31102da
tejas-oza <ozatejas01@gmail.com>
2025-03-09 00:10:21 +0530

    add common middlewares and usefull environmen variables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import dotenv from "dotenv";
 import { app } from "./app.js";
-const PORT = 4000;
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
